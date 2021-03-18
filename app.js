b561c1dce5a7d0514c0c24b02204c990a481ad29
Vishruth Shandilya <vishruthshandilya1989@gmail.com>
2021-03-18 10:22:20 +0530

    fixed

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://bhairava:Kala1998@cluster0.zljsn.mongodb.net/node-jwt?retryWrites=true&w=majority';
+const dbURI = process.env.MONGODB_URI;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
